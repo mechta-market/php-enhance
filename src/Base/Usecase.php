e3158c789588b4aa394c6fc9c67caf12d3281fcc
Rustembek Kaliyev <kaliyev.r@gmail.com>
2024-06-18 12:26:23 +0500

    Usecase.php setInput method

--- src/Base/Usecase.php
+++ src/Base/Usecase.php
@@ -10,15 +10,18 @@ abstract class Usecase
     private Output $output;
     private Input $input;
 
-    public function __construct(Input $input){
+    public function __construct(){
         $this->output = new Output();
-        $this->input = $input;
     }
 
     final public function getOutput(): Output {
         $this->output->setErrors($this->getErrorsMessages());
 
         return $this->output;
+    }
+
+    final public function setInput(Input $input): void {
+        $this->input = $input;
     }
 
     abstract public function execute(): void;
